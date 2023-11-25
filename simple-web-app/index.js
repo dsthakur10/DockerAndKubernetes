654ef1a177f6bbf55e64e34b14f8508cc185dbf0
Devendra <devendrasingh.thakur@ibm.com>
2023-11-26 01:00:42 +0530

    Unnecessary rebuilds of NPM install

--- simple-web-app/index.js
+++ simple-web-app/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hey there, Warrior");
+    res.send("Bye bye, Warrior");
 });
 
 app.listen(8888, () => {

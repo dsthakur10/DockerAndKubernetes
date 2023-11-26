a1be2c95cac9cbce6ddab1c32f642d7f1388473b
Devendra <devendrasingh.thakur@ibm.com>
2023-11-27 00:41:57 +0530

    Restart policies in Docker-Compose in case of crashing of container

--- count-visits-web-app/index.js
+++ count-visits-web-app/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 const app = express();
 const client = redis.createClient({
@@ -15,6 +16,9 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get("/", (req, res) => {
+
+    process.exit(0); // to check restart container options of Docker-compose
+
     client.get('visits', (err, visits) => {
         res.send("Number of visits = " + visits);
         client.set('visits', parseInt(visits) + 1);
